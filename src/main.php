db659a080c1b91def423b52ca5cb429b8c91f65a
Kevin Wu <kvmisc@163.com>
2020-02-20 10:32:57 +0800

    dffas

--- src/main.php
+++ src/main.php
@@ -10,8 +10,8 @@ $provider = new HttpProvider(new HttpRequestManager('https://ropsten.infura.io/v
 
 $web3 = new Web3($provider);
 
-$private_key_1 = 'ce4f6073137aea12e8459e996792cc8d50fc0dee36d615e0b7c129e7dd47bb57';
-$public_key_1 = '0x07d50c70893030529a82964a3f15443b9fdf0687';
+$private_key_1 = 'AEB0AFB0FA00351A234C43F4F65CAA6DAFAB89602F53C21CC79C718AC1FB6D8E';
+$public_key_1 = '0xD2E85464C5Ad682161b2df85249204D6F00142e9';
 
 
 // $eth = $web3->eth;
@@ -26,8 +26,8 @@ $public_key_1 = '0x07d50c70893030529a82964a3f15443b9fdf0687';
 // });
 
 use Web3\Contract;
-$contract_abi = '[{"constant":false,"inputs":[{"name":"mid","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"coordination","type":"uint256"}],"name":"createCoordination","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"mid","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"index","type":"uint256"}],"name":"deleteCoordination","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"mid","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"coordination","type":"uint256"},{"name":"index","type":"uint256"}],"name":"updateCoordination","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"mid","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"index","type":"uint256"}],"name":"readCoordination","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]';
-$contract_addr = '0x740863737963b7a6ddc63c86ad2c88c7b3c8a1a2';
+$contract_abi = '[{"constant":false,"inputs":[{"name":"mid","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"coordination","type":"uint256"}],"name":"createCoordination","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"mid","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"index","type":"uint256"}],"name":"deleteCoordination","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"mid","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"coordination","type":"uint256"},{"name":"index","type":"uint256"}],"name":"updateCoordination","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"mid","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"index","type":"uint256"}],"name":"readCoordination","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]';
+$contract_addr = '0x9EB0244C04adFC9795BDba985B550c3690a0Af89';
 $contract = new Contract($provider, $contract_abi);
 // $contract->at($contract_addr)->call('readCoordination', 1,2,0, function ($err, $res) {
 //     if ($err !== null) {
--- src/main.sol
+++ src/main.sol
@@ -1,22 +1,32 @@
 pragma solidity ^0.4.18;
 
 contract FlightDB {
+    address public manager;
+
     // machine_id -> day_1 -> coordination_1
     //                        coordination_2
     //            -> day_2 -> coordination_1
     //                        coordination_2
     mapping(uint => mapping(uint => uint[])) db;
 
+    constructor() public {
+        manager = msg.sender;
+    }
+
     function createCoordination(uint mid, uint timestamp, uint coordination) public {
+        require(msg.sender == manager, "not allowed");
         db[mid][timestamp].push(coordination);
     }
     function readCoordination(uint mid, uint timestamp, uint index) public view returns (uint) {
+        require(msg.sender == manager, "not allowed");
         return db[mid][timestamp][index];
     }
     function updateCoordination(uint mid, uint timestamp, uint coordination, uint index) public {
+        require(msg.sender == manager, "not allowed");
         db[mid][timestamp][index] = coordination;
     }
     function deleteCoordination(uint mid, uint timestamp, uint index) public {
+        require(msg.sender == manager, "not allowed");
         uint[] storage list = db[mid][timestamp];
         uint length = list.length;
         if ( length > 0 ) {
